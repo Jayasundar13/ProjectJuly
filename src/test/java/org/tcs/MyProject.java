042af38a232cf6f7681eb0f3ddeebca66d2607e1
jayasundar13 <hotnfreeze@gmail.com>
2020-07-19 22:25:50 +0530

    main added

--- src/test/java/org/tcs/MyProject.java
+++ src/test/java/org/tcs/MyProject.java
@@ -1,5 +1,12 @@
 package org.tcs;
 
 public class MyProject {
-
+	
+	private void courseName() {
+		System.out.println("My name is Jai");
+	}
+	public static void main(String[] args) {
+		MyProject pt = new MyProject();
+		pt.courseName();
+	}
 }
